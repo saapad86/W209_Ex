b375929b2606aaef60db8e889f2810557a1a493f
saapad86 <saad.padela@gmail.com>
2016-08-20 13:11:46 -0400

    added interactivity

--- bar.js
+++ bar.js
@@ -63,6 +63,71 @@ d3.csv("bar-data.csv", function(error, data) {
       .attr("x", function(d) { return x(d.date); })
       .attr("width", x.rangeBand())
       .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
+      .attr("height", function(d) { return height - y(d.value); })
+      .append("svg:title")
+      .text(function(d) { return d.value + "  steps taken on " + d.date; });
 
 });
+
+var toggleColor = (function(){
+   var currentColor = "steelblue";
+
+    return function(){
+        currentColor = currentColor == "steelblue" ? "orange" : "steelblue";
+        d3.select(this).style("fill", currentColor);
+    }
+})();
+
+var highlightData = (function() {
+  d3.select("svg").remove();
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+  d3.csv("bar-data.csv", function(error, data) {
+
+      data.forEach(function(d) {
+          d.date = parseDate(d.date);
+          d.value = +d.value;
+      });
+    
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "1em")
+        .attr("transform", "rotate(-45)" );
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("# of Steps");
+
+    svg.selectAll("bar")
+        .data(data)
+      .enter().append("rect")
+        .style("fill", "steelblue")
+        .attr("x", function(d) { return x(d.date); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
+        .on("click",toggleColor)
+      .filter(function(d) { return d.value < 4000 })
+        .style("fill","orange");
+
+  });  
+})
